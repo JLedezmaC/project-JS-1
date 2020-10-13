db9b33f54595240aa49f076109a8b16a71a9a883
Juan Ignacio <>
2020-10-12 18:50:26 -0600

    Agrego Proyeto JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 function initAccordion(accordionElem){
 
-  function handlePanelClick(event){
-    showPanel(event.currentTarget);
-  }  
   /* Al expanded panel que es el div que cubre todo si no es null se le remueve el active y al panel que es el div actual se le agrega el active*/ 
 
   function showPanel(panel){
       let expandedPanel = accordionElem.querySelector(".active");
       if (expandedPanel){
         expandedPanel.classList.remove("active");
-      } 
-      panel.classList.add("active");
+      }else{
+        panel.classList.add("active");
+      }
   }
-
   let allPanelElems = accordionElem.querySelectorAll(".panel");
   for (let i = 0; i < allPanelElems.length; i++){
-    allPanelElems[i].addEventListener("click", handlePanelClick);
+    allPanelElems[i].addEventListener("click", (event) => {
+      showPanel(event.currentTarget);
+    });
   }
-  showPanel(allPanelElems[0])
 }
 
 initAccordion(document.getElementById("accordion"));
+
